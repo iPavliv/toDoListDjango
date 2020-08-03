45cf0b661bd7bad550040170a226b7bfb144d5d9
iPavliv <iryna.pavliv96@gmail.com>
2020-08-03 19:03:33 +0300

    Add test task model

--- toDoList/tasks/models.py
+++ toDoList/tasks/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    """
+    A model for task class.
+    """
+    task_text = models.TextField()
+    comment = models.TextField()
+    create_date = models.DateTimeField()
+    is_done = models.BooleanField()
+
+    class Meta:
+        ordering = ["-create_date"]
+
+    def __str__(self):
+        """
+        String for representing the Task object.
+        """
+        return self.task_text
